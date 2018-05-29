70ede4ac4c628ee963d6e0a56117f2354baadb3a
Soumen Saniel <soumen.saniel@gmail.com>
2018-05-29 17:27:52 +0530

    parsing http method

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const http = require('http')
 const url = require('url')
-
+// initialize the server
 const server = http.createServer((req, res) => {
+  // get url and parse it
   const parsedUrl = url.parse(req.url, true)
+  // get path
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+  // get the http method
+  const method = req.method.toLowerCase()
+  // send response
   res.end('Hello World!\n')
-  console.log('Request recived on path ', trimmedPath)
+  console.log('Request recived on path ' + trimmedPath + ' with method: ' + method)
 })
 
 server.listen(3000, () => {
